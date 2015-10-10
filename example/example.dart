86833155ca12bbb8cbc1d31305cfeadc5beef630
mezoni <andrew.mezoni@gmail.com>
2015-10-10 16:14:35 +0500

    0.0.1

--- example/example.dart
+++ example/example.dart
@@ -1,14 +1,3 @@
-# semaphore
-
-Semaphore is lightweight data type that is used for controlling access to a common resource inside isolate.
-
-Version: 0.0.1
-
-The goal of the Dash effort is ultimately to replace JavaScript as the lingua franca of web development on the open web platform.
-
-### Example:
-
-```dart
 import 'dart:async';
 
 import 'package:semaphore/semaphore.dart';
@@ -46,4 +35,3 @@ Future _doWork(int ms) {
   // Simulate work
   return new Future.delayed(new Duration(milliseconds: ms));
 }
-```
